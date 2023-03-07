d0559b1050ff4afdf89717b495236837cdedc4b4
pierre jebara <pierrehenrijebara@gmail.com>
2023-03-07 11:39:32 +0100

    controllers and models with validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  validates :phone_number, format: { with: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/}
+  has_many :contacts
+  has_many :routes
+  has_many :addresses, through: :routes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
---
+++ db/migrate/20230307101019_add_attributes_to_users.rb
@@ -0,0 +1,9 @@
+class AddAttributesToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :photo, :string
+    add_column :users, :location, :string
+  end
+end
