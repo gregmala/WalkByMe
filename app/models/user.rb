889a5d013016ddb1196f95797a4630b00c64be40
pierre jebara <pierrehenrijebara@gmail.com>
2023-03-07 12:01:31 +0100

    added validations for phone numbers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-  validates :phone_number, format: { with: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/}
+  validates :phone_number, format: { with: /\A\+?\d{1,4}[-.\s]?\d{6,14}\z/ }
   has_many :contacts
   has_many :routes
   has_many :addresses, through: :routes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+        :recoverable, :rememberable, :validatable
 end
